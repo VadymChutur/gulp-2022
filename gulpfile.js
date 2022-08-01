6ffb53ee501b61ba71e2a1a7f5b0eaf72cafa655
Vadym Chutur <falloutgreck@gmail.com>
2022-08-01 13:57:02 +0300

    add gulp add include-file add  del add copy

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+//main module
+import gulp from 'gulp';
+
+//import path
+import { path } from './gulp/config/path.js';
+
+//global const
+global.app = {
+  path: path,
+  gulp: gulp,
+};
+
+//import task
+import { copy } from './gulp/tasks/copy.js';
+import { reset } from './gulp/tasks/reset.js';
+import { html } from './gulp/tasks/html.js';
+
+//watching recive in files
+function watcher() {
+  gulp.watch(path.watch.files, copy);
+  gulp.watch(path.watch.html, html);
+}
+
+const mainTasks = gulp.parallel(copy, html);
+
+//build scenarios in progres task
+const dev = gulp.series(reset, mainTasks, watcher);
+
+//implementation default scenary
+gulp.task('default', dev);
